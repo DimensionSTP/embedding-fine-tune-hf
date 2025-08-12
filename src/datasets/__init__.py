467dea867e694dc1d7a2dc6f732d77bc6aa10eee
DimensionSTP <ddang8jh@gmail.com>
2025-08-12 15:40:46 +0900

    feat: only use structural dataset classes

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,11 +1,7 @@
 from .triplet_dataset import StructuralDataset as TripletStructuralDataset
-from .triplet_dataset import ConversationalDataset as TripletConversationalDataset
 from .test_dataset import StructuralDataset as TestStructuralDataset
-from .test_dataset import ConversationalDataset as TestConversationalDataset
 
 __all__ = [
     "TripletStructuralDataset",
-    "TripletConversationalDataset",
     "TestStructuralDataset",
-    "TestConversationalDataset",
 ]
