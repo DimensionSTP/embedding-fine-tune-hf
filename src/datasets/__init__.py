0e10a80799cf54739975b647ac43c797c4466346
DimensionSTP <ddang8jh@gmail.com>
2025-10-13 19:09:28 +0900

    delete: unused module init

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,7 +1,5 @@
 from .triplet_dataset import StructuralDataset as TripletStructuralDataset
-from .test_dataset import StructuralDataset as TestStructuralDataset
 
 __all__ = [
     "TripletStructuralDataset",
-    "TestStructuralDataset",
 ]
