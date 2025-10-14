daf4c4447e8bae945bf3d4e1aa869921a1b124b1
DimensionSTP <ddang8jh@gmail.com>
2025-10-14 19:29:53 +0900

    chore: args in default scripts to sync with main config

--- scripts/upload_all_to_hf_hub.sh
+++ scripts/upload_all_to_hf_hub.sh
@@ -5,9 +5,9 @@ dataset_name="triplet"
 strategy="sentence_transformers"
 model_detail="Qwen3-Embedding-4B"
 upload_tag="triplet"
-is_peft=True
-batch_size=4
-gradient_accumulation_steps=4
+is_peft=False
+batch_size=16
+gradient_accumulation_steps=1
 
 python $path/upload_all_to_hf_hub.py \
     dataset_name=$dataset_name \
--- scripts/upload_to_hf_hub.sh
+++ scripts/upload_to_hf_hub.sh
@@ -5,10 +5,10 @@ dataset_name="triplet"
 strategy="sentence_transformers"
 model_detail="Qwen3-Embedding-4B"
 upload_tag="triplet"
-is_peft=True
-batch_size=4
-gradient_accumulation_steps=4
-step=30
+is_peft=False
+batch_size=16
+gradient_accumulation_steps=1
+step=100
 
 python $path/upload_to_hf_hub.py \
     dataset_name=$dataset_name \
